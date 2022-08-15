7ed13fa36dca874c52fc4307b143a2351f5801ec
TH-Raju <rajukhan894200@gmail.com>
2022-08-15 23:28:52 +0600

    simple Bank Site Developed using JS Dom

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,6 +3,16 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawFieldSt = withdrawField.value;
     const newWithdrawField = parseFloat(newWithdrawFieldSt);
 
+
+    //input clear
+    withdrawField.value = '';
+
+
+    if (isNaN(newWithdrawField)) {
+        alert('Please Provide a valid Number!');
+        return;
+    }
+
     const currentWithdrawElement = document.getElementById('withdraw-total');
     const previousWithdawTotalSt = currentWithdrawElement.innerText;
     const previousWithdawTotal = parseFloat(previousWithdawTotalSt);
@@ -22,6 +32,11 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const previousBalanceTotalSt = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalSt);
 
+    if (newWithdrawField > previousBalanceTotal) {
+        alert("There is not Enough Money");
+        return;
+    }
+
     //calculate new balance
 
     const currentBalanceTotal = previousBalanceTotal - newWithdrawField;
@@ -33,6 +48,5 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
 
 
 
-    //input clear
-    withdrawField.value = '';
+
 })
